49e8e791cda49a92368896de8d2dc2ce77976d9a
Terrence Chung <terrencechung35@gmail.com>
2023-04-09 22:36:27 -0700

    fixed localstorage stuff

--- client/src/components/Blackjack.js
+++ client/src/components/Blackjack.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
-// import { useParams} from "react-router-dom";
 import { Box, Button } from "../styles";
 
 const backCard = 'https://deckofcardsapi.com/static/img/back.png'
@@ -21,45 +20,46 @@ function Blackjack({ user }) {
   const [isGameOver, setIsGameOver] = useState(false);
   const [game, setGame] = useState([])
 
-  const loadGameStateFromLocalStorage = () => {
-    const gameState = JSON.parse(localStorage.getItem('blackjack-game')) || {
-      cards: [],
-      dealerHand: [],
-      userHand: [],
-      gameStart: false,
-      gameResult: 'In Progress',
-    };
-
-    setCards(gameState.cards);
-    setDealerHand(gameState.dealerHand);
-    setUserHand(gameState.userHand);
-    setGameStart(gameState.gameStart !== undefined ? gameState.gameStart : !!gameState.userHand.length);
-    setGameResult(gameState.gameResult);
-  };
+  useEffect(() => {
+    const gameState = JSON.parse(localStorage.getItem('blackjack-game'));
+    if (gameState) {
+      setCards(gameState.cards);
+      setDealerHand(gameState.dealerHand);
+      setUserHand(gameState.userHand);
+      // setGameStart(gameState.gameStart !== undefined ? gameState.gameStart : !!gameState.userHand.length);
+      setGameResult(gameState.gameResult);
+      setIsGameOver(gameState.isGameOver)
+      setGame(gameState.game);
+    }
+  }, []);
 
   // Call the loadGameStateFromLocalStorage function whenever the component mounts
-  useEffect(() => {
-    loadGameStateFromLocalStorage();
-  }, []);
+  // useEffect(() => {
+  //   loadGameStateFromLocalStorage();
+  // }, []);
 
   useEffect(() => {
     const storedGame = JSON.parse(localStorage.getItem('blackjack-game'))?.game;
     if (storedGame) {
       setGame(storedGame);
+      setGameStart(true);
     }
   }, []);
   // Save the game state to localStorage whenever it changes
   useEffect(() => {
-    const gameState = {
-      cards,
-      dealerHand,
-      userHand,
-      gameStart,
-      gameResult,
-      game,
-    };
-    localStorage.setItem('blackjack-game', JSON.stringify(gameState));
-  }, [cards, dealerHand, userHand, gameStart, gameResult, game]);
+    if (game) {
+      const gameState = {
+        cards,
+        dealerHand,
+        userHand,
+        gameStart,
+        gameResult,
+        game,
+        isGameOver,
+      };
+      localStorage.setItem('blackjack-game', JSON.stringify(gameState));
+    }
+  }, [cards, dealerHand, userHand, gameResult, isGameOver, game]);
 
 
   useEffect(() => {
@@ -78,6 +78,7 @@ function Blackjack({ user }) {
   console.log("gameStart",gameStart);
 
   async function startNewGame() {
+    // localStorage.removeItem('blackjack-game');
     const newDealerHand = [cards.pop(), '*'];
     const newUserHand = [cards.pop(), cards.pop()];
 
@@ -257,124 +258,3 @@ const Wrapper = styled.section`
   `;
 
 export default Blackjack;
-
-
-// useEffect(()=>{
-
-  //   fetch('/games', {
-    //       method: 'POST',
-    //       headers: { 'Content-Type': 'application/json' },
-    //       body: JSON.stringify({
-      //           user_id: user.id,
-      //           dealer_hand: dealerHand,
-      //           user_hand: userHand,
-      //         })
-      //       });
-      // },[]);
-
-
-
-      // async function hit() {
-        //   const newUserHand = [...userHand];
-        //   newUserHand.push(cards.pop());
-        //   setUserHand(newUserHand);
-        //   if (calculateHandValue(newUserHand) > 21) {
-          //     setIsGameOver(true);
-          //   }
-          //   await fetch('/games', {
-            //     method: 'PUT',
-            //     headers: { 'Content-Type': 'application/json' },
-            //     body: JSON.stringify({ user_hand:newUserHand })
-            //   });
-            // }
-
-            // async function stand() {
-              //   const newDealerHand = [...dealerHand];
-              //   newDealerHand[1] = cards.pop(); // reveal the second card
-              //   while (calculateHandValue(newDealerHand) < 17) {
-                //     newDealerHand.push(cards.pop());
-                //   }
-                //   setDealerHand(newDealerHand);
-                //   setIsGameOver(true);
-                // }
-                //   await fetch('/cards', {
-                  //     method: 'PUT',
-                  //     headers: { 'Content-Type': 'application/json' },
-                  //     body: JSON.stringify({ cards })
-                  //   });
-
-                  // function calculateHandValue(cards) {
-                    //   let sum = 0;
-                    //   let numAces = 0;
-                    //   cards.forEach(card => {
-                      //     const value = card.value;
-                      //     if (value === 11) {
-                        //       numAces++;
-                        //     }
-                        //     sum += value;
-                        //   });
-                        //   while (sum > 21 && numAces > 0) {
-                          //     sum -= 10;
-                          //     numAces--;
-                          //   }
-                          //   return sum;
-                          // }
-
-                          // function getGameResult() {
-                            //   const userValue = calculateHandValue(userHand);
-                            //   const dealerValue = calculateHandValue(dealerHand);
-
-                            //   if (isGameOver) {
-                              //     if (userValue > 21) {
-                                //       return 'You went over 21. You lose.';
-                                //     } else if (dealerValue > 21) {
-                                  //       return 'Dealer went over 21. You win!';
-                                  //     } else if (userValue > dealerValue) {
-                                    //       return 'You win!';
-                                    //     } else if (dealerValue > userValue) {
-                                      //       return 'You lose.';
-                                      //     } else {
-                                        //       return 'It\'s a tie!';
-                                        //     }
-                                        //   }
-                                        // }
-
-
-                                        // return (
-                                          // <Wrapper>
-                                          //   <h1>Hello</h1>
-                                          // </Wrapper>
-                                          //   <Wrapper>
-                                          //     {isGameOver ? (
-                                            //       <Box>
-                                            //         <p>Dealer: {dealerHand.join(', ')}</p>
-                                            //         <p>User: {userHand.join(', ')}</p>
-                                            //         {/* <p>Sum of user's cards: {calculateHandValue(userHand)}</p> */}
-                                            //         {/* <p>{getGameResult()}</p> */}
-                                            //         {/* <Button onClick={getCards}>Deal again</Button> */}
-                                            //       </Box>
-                                            //     ) : (
-                                              //       <Box>
-                                              //         <p>Dealer: {dealerHand[0]} _</p>
-                                              //         <p>User: {userHand.join(', ')}</p>
-                                              //         {/* <p>Sum of user's cards: {calculateHandValue(userHand)}</p> */}
-                                              //         {/* <Button onClick={hit}>Hit</Button>
-                                            //         <Button onClick={stand}>Stand</Button> */}
-                                            //       </Box>
-                                            //     )}
-                                            //     {/* <p>Remaining cards: {cards.length}</p> */}
-                                            //   </Wrapper>
-                                            // );
-                                            /* {isGameOver ? (
-                                              <Box> */
-                                                /* <p>{gameResult}</p> */
-                                                /* <Button onClick={getCards}>Deal again</Button> */
-                                              /* </Box>
-                                            ) : (
-                                              <Box>
-                                                <p>Dealer: {dealerHand[0]} _</p>
-                                                <p>User: {userHand.join(', ')}</p> */
-                                                /* <Button onClick={hit}>Hit</Button>
-                                                <Button onClick={stand}>Stand</Button> */
-                                              /* </Box>
-                                            )} */
